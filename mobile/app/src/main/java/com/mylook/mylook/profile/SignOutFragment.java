22867c27fec70defd4f8bed8430fd9ea6cf8ffe0
M-Exequiel <mark_lopez1996@hotmail.com>
2018-09-17 18:35:19 -0300

    Esto es lo que hice del perfil d la tienda

---
+++ mobile/app/src/main/java/com/mylook/mylook/profile/SignOutFragment.java
@@ -0,0 +1,20 @@
+package com.mylook.myapp.Window;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mylook.myapp.R;
+
+public class SignOutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_sign_out,container,false);
+        return view;
+    }
+}
