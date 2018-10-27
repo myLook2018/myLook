89d8423cbd56abc9adf29f571ea215610cb21f46
A-Donato <alexis.donato95@gmail.com>
2018-10-27 17:51:45 -0300

    get() articles async, datos modificados

--- web/src/app/service/dataService.ts
+++ web/src/app/service/dataService.ts
@@ -22,7 +22,7 @@ export class DataService {
             if (file.type.split('/')[0] !== 'image') {
                 return reject('Tipo de imagen no soportado');
             }
-            const path = `test/${new Date().getTime()}_${file.name}`;
+            const path = `articles/${new Date().getTime()}_${file.name}`;
             const customMetadata = { app: 'Mylook!' };
             this.task = this.storage.upload(path, file, { customMetadata });
             this.ref = this.storage.ref(path);
@@ -45,7 +45,7 @@ export class DataService {
             if (file.type.split('/')[0] !== 'image') {
                 return reject('Tipo de imagen no soportado');
             }
-            const path = `test/${new Date().getTime()}_${file.name}`;
+            const path = `article/${new Date().getTime()}_${file.name}`;
             const customMetadata = { app: 'Mylook!' };
             this.task = this.storage.upload(path, file, { customMetadata });
             this.ref = this.storage.ref(path);
