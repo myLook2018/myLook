323979a8ceb82aecf5333edc2b8c198a93e70f3c
Donato <Alexis_Donato@McAfee.com>
2019-03-21 17:48:24 -0300

    appguard mejorado

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -7,12 +7,8 @@ import { UserResolver } from './auth/components/user/user.resolver';
 import { AuthGuard } from './auth/services/auth.guard';
 import { InventoryComponent } from './articles/components/inventory/inventory.component';
 import { SignupComponent } from './auth/components/signup/signup.component';
-import { StoreService } from './auth/services/store.service';
 import { StoreComponent } from './store/components/store/store.component';
-// import { StoreResolver, ArticleResolver } from './store/components/store/store.resolver';
-import { ErrorComponent } from './error/error.component';
 import { RecomendationsComponent } from './recomendations/components/recomendations.component';
-import { DonutchartComponent } from './anylitics/components/donutchart/donutchart.component';
 import { DashboardComponent } from './anylitics/components/dashboard/dashboard.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
@@ -21,20 +17,20 @@ export const rootRouterConfig: Routes = [
  // { path: '', component: HomePageComponent},
 
   { path: '', pathMatch: 'full' ,redirectTo: '/Inicio'},
-  { path: 'Inicio', component: HomePageComponent},
-  { path: '', runGuardsAndResolvers: 'always',
+  { path: 'Inicio', component: HomePageComponent, canActivate: [AuthGuard] },
+  { path: '', canActivate: [AuthGuard], runGuardsAndResolvers: 'always', 
     children: [
-      { path: 'Tiendas/:storeName', component: StoreComponent, resolve: { data: UserResolver }},
-      { path: 'Tiendas/:storeName/Inventario', component: InventoryComponent, resolve: { data: UserResolver }},
-      { path: 'Tiendas/:storeName/Estadisticas', component: DashboardComponent, resolve: { data: UserResolver }},
-      { path: 'Tiendas/:storeName/Recomendaciones', component: RecomendationsComponent, resolve: { data: UserResolver }},
+      { path: 'Tiendas/:storeName', component: StoreComponent, resolve: { data: UserResolver }, canActivate: [AuthGuard]},
+      { path: 'Tiendas/:storeName/Inventario', component: InventoryComponent, resolve: { data: UserResolver }, canActivate: [AuthGuard]},
+      { path: 'Tiendas/:storeName/Estadisticas', component: DashboardComponent, resolve: { data: UserResolver }, canActivate: [AuthGuard]},
+      { path: 'Tiendas/:storeName/Recomendaciones', component: RecomendationsComponent, resolve: { data: UserResolver }, canActivate: [AuthGuard]},
     ]},
-  { path: 'Ingresar', component: LoginComponent, canActivate: [AuthGuard] },
-  { path: 'Registrarse', component: SignupComponent, canActivate: [AuthGuard] },
-  { path: 'user', component: UserComponent, resolve: { data: UserResolver } },
-  { path: 'Registrar Tienda', component: RegisterComponent },
+  { path: 'Ingresar', component: LoginComponent, canActivate: [AuthGuard]},
+  { path: 'Registrarse', component: SignupComponent, canActivate: [AuthGuard]},
+  { path: 'user', component: UserComponent, resolve: { data: UserResolver }},
+  { path: 'Registrar Tienda', component: RegisterComponent, canActivate: [AuthGuard]},
   
-  { path: '404', component: ErrorComponent },
+  { path: '**', redirectTo:'/Inicio'},
 ];
 
 @NgModule({
--- web/src/app/auth/services/auth.guard.ts
+++ web/src/app/auth/services/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, Router} from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { UserService } from './user.service';
-
+import { DataService } from '../../service/dataService';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -10,18 +10,33 @@ export class AuthGuard implements CanActivate {
   constructor(
     public afAuth: AngularFireAuth,
     public userService: UserService,
-    private router: Router
+    private router: Router,
+    public dataService: DataService
   ) {}
 
   canActivate(): Promise<boolean> {
     return new Promise((resolve, reject) => {
-      this.userService.getCurrentUser()
-      .then(user => {
-        this.router.navigate(['/home']);
+      const user = this.dataService.getStoreInfo();
+      if(user.firebaseUID !== '' && !this.router.url.toString().includes('Tiendas')) {
+        console.log("*********************************** esta logeado y con url -" , this.router.url)
+        this.router.navigate(['Tiendas', user.storeName]);
         return resolve(false);
-      }, err => {
-        return resolve(true);
-      });
+      } else {
+        if( user.firebaseUID !== '' && this.router.url.toString().includes('Tiendas')){
+          console.log("*********************************** esta logeado y con url +" , this.router.url)
+          return resolve(true);
+        } else {
+          if( user.firebaseUID === '' && this.router.url.toString().includes('Tiendas')) {
+            console.log("*********************************** esta logeado y con url +" , this.router.url)
+            this.router.navigate(['Inicio']);
+            return resolve(false);
+          } else {
+            console.log("*********************************** no esta logueado y no va a tienda")
+            return resolve(true);
+          }
+
+        }
+      }
     });
   }
 }
