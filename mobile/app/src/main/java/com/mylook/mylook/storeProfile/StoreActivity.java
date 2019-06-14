727318019d73c2a6268090415195ecbd4c626639
Marcos Lopez <mark_lopez1996@hotmail.com>
2019-06-14 17:29:04 -0300

    Update StoreActivity

--- mobile/app/src/main/java/com/mylook/mylook/storeProfile/StoreActivity.java
+++ mobile/app/src/main/java/com/mylook/mylook/storeProfile/StoreActivity.java
@@ -1,27 +1,20 @@
 package com.mylook.mylook.storeProfile;
 
-import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.mylook.mylook.R;
