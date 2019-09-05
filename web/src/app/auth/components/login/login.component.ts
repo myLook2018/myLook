c84ca28f02b4374f58f902abe2697d95d99cd74c
Alexis Donato <Alexis.donato95@gmail.com>
2019-09-05 20:19:13 -0300

    Bug/ml 056/reestablecer contraseña no anda (#119)
    * ahora anda giseeeee. esto es una feature no un bug, nunca la habia implementado

--- web/src/app/auth/components/login/login.component.ts
+++ web/src/app/auth/components/login/login.component.ts
@@ -6,6 +6,7 @@ import {FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import {UserService} from '../../services/user.service';
 import { Subscription } from 'rxjs';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -27,6 +28,7 @@ export class LoginComponent implements OnDestroy{
   matcher = new MyErrorStateMatcher();
 
   constructor(
+    public snackBar: MatSnackBar,
     public userService: UserService,
     public authService: AuthService,
     private router: Router,
@@ -113,6 +115,48 @@ export class LoginComponent implements OnDestroy{
       console.log(err);
       this.errorMessage = this.translateError(err);
       this.isLoading = false;
+    });
+  }
+
+  translateError(error: string) {
+    let message = '';
+    switch (true) {
+      case (error.includes('password is invalid')):
+        message = 'Constraseña incorrecta.';
+        break;
+      case (error.includes('no user record')):
+        message = 'El email ingresado no se encuentra registrado en myLook.';
+        break;
+      case (error.includes('many unsuccessful login attempts')):
+        // tslint:disable-next-line: max-line-length
+        message = 'Verifica por favor los datos ingresados. Si los intentos fallidos continúan, bloquearemos temporalmente tu cuenta por seguridad.';
+        break;
+      case (error.includes('auth/user-not-found')):
+        message = 'No se ha encontrado usuario registrado con ese email, verifica los datos ingresados.';
+        break;
+      case (error.includes('auth/invalid-email')):
+        message = 'Email ingresado invalido.';
+        break;
+      default:
+        message = error;
+        break;
+    }
+    return message;
+  }
+
+  restartPassword() {
+    this.authService.sendResetPasswordEmail(this.loginForm.get('email').value).then( res => {
+      console.log('res', res);
+      this.openSnackBar('Te hemos enviado un email para reestablecer tu contraseña!', 'cerrar');
+    }).catch(error => {
+      console.log('el error', error);
+      this.openSnackBar(this.translateError(error.code), 'cerrar');
+    });
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000
     });
   }
 
